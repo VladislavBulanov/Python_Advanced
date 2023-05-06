be6cffac4339a7e2669442742ac6fdb508314196
Vladislav <vladislav.bulanov1895@gmail.com>
2023-05-06 18:40:37 +0300

    Done: first application

--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -1,10 +1,24 @@
 import datetime
 from flask import Flask
 
+
 app = Flask(__name__)
+total_calls = 0
 
 
 @app.route('/test')
 def test_function():
     now = datetime.datetime.now().utcnow()
     return f'Это тестовая страничка, ответ сгенерирован в {now}'
+
+
+@app.route('/hello/world')
+def print_hello_world():
+    return 'Hello, world!'
+
+
+@app.route('/counter')
+def show_total_calls():
+    global total_calls
+    total_calls += 1
+    return str(total_calls)
