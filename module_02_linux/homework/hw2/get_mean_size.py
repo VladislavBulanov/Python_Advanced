93c1d0fb542fa81696508a78cae54b14ec3dd5ab
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-09 16:52:25 +0300

    Done: task n.2

--- module_02_linux/homework/hw2/get_mean_size.py
+++ module_02_linux/homework/hw2/get_mean_size.py
@@ -1,20 +1,26 @@
-"""
-Удобно направлять результат выполнения команды напрямую в программу с помощью конвейера (pipe):
-
-$ ls -l | python3 get_mean_size.py
-
-Напишите функцию get_mean_size, которая на вход принимает результат выполнения команды ls -l,
-а возвращает средний размер файла в каталоге.
-"""
-
 import sys
+from typing import List, Union
 
 
-def get_mean_size(ls_output: str) -> float:
-    ...
+def get_mean_size(ls_output: List[str]) -> Union[float, str]:
+    """
+    The function receives data from output file and
+    returns mean size of file in initial directory
+    or error message if initial directory is empty
+    or data cannot be retrieved.
+    :param ls_output: data from output file
+    """
+    if ls_output:
+        try:
+            total_sum = sum(float(line.split()[4])for line in ls_output)
+            return round(total_sum / len(ls_output), 2)
+        except BaseException:
+            return 'Что-то пошло не так...'
+
+    return 'В данной директории нет файлов.'
 
 
 if __name__ == '__main__':
-    data: str = sys.stdin.read()
+    data: List[str] = sys.stdin.readlines()[1:]
     mean_size: float = get_mean_size(data)
     print(mean_size)
