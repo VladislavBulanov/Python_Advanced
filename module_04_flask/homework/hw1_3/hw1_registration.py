fd4722222501e0de6944728b79f137b43fed69d2
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-19 17:23:46 +0300

    Done: task n.1

--- module_04_flask/homework/hw1_3/hw1_registration.py
+++ module_04_flask/homework/hw1_3/hw1_registration.py
@@ -1,29 +1,34 @@
-
-"""
-В эндпоинт /registration добавьте все валидаторы, о которых говорилось в последнем видео:
-
-1) email (текст, обязательно для заполнения, валидация формата);
-2) phone (число, обязательно для заполнения, длина — десять символов, только положительные числа);
-3) name (текст, обязательно для заполнения);
-4) address (текст, обязательно для заполнения);
-5) index (только числа, обязательно для заполнения);
-6) comment (текст, необязательно для заполнения).
-"""
-
 from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
+from wtforms.validators import InputRequired, Email, NumberRange
 from hw2_validators import number_length, NumberLength
 
 app = Flask(__name__)
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField()
-    phone = IntegerField()
-    name = StringField()
-    address = StringField()
-    index = IntegerField()
+    email = StringField(validators=[
+        InputRequired("The field 'email' is required"),
+        Email("The field 'email' is invalid format"),
+    ])
+    phone = IntegerField(validators=[
+        InputRequired("The field 'phone' is required"),
+        NumberRange(
+            min=1000000000,
+            max=9999999999,
+            message="The phone number must consist of ten digits",
+        ),
+    ])
+    name = StringField(validators=[
+        InputRequired("The field 'name' is required"),
+    ])
+    address = StringField(validators=[
+        InputRequired("The field 'address' is required"),
+    ])
+    index = IntegerField(validators=[
+        InputRequired("The field 'index' is required"),
+    ])
     comment = StringField()
 
 
