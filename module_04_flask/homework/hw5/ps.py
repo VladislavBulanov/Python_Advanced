df2bf04f9d701ea2db5640ae131efe791eb05a75
Vladislav Bulanov <solist95@mail.ru>
2023-05-22 14:51:45 +0300

    done: task n.5

--- module_04_flask/homework/hw5/ps.py
+++ module_04_flask/homework/hw5/ps.py
@@ -1,45 +1,24 @@
-from flask import Flask, request
-from subprocess import run
-from typing import List
 import shlex
+
+from flask import Flask, request
+from subprocess import run, PIPE
+from typing import List
+
 
 app = Flask(__name__)
 
 
 @app.route('/ps', methods=['GET'])
 def execute_ps() -> str:
-    """The function returns result of performing 'ps' command."""
+    """The function returns result of performing 'ps' command with arguments."""
 
-    args_list: List[str] = request.args.getlist('arg')
-    args_string: str = shlex.quote("".join(args_list))
-    command_str = f"ps {args_string}"
-    command = shlex.split(command_str)
-    result = run(command, capture_output=True)
-    return f"<pre>{result}</pre>"
+    args: List[str] = request.args.getlist('arg')
+    clean_args: List[str] = [shlex.quote(arg) for arg in args]
+    command = ['ps'] + clean_args
+    result = run(command, stdout=PIPE, stderr=PIPE, encoding='utf-8')
+    output = result.stdout.strip()
+    return f"<pre>{output}</pre>"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-# from flask import Flask, request
-# from subprocess import run, PIPE
-# from typing import List
-# import shlex
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/ps', methods=['GET'])
-# def execute_ps() -> str:
-#     args: List[str] = request.args.getlist('arg')
-#     command = ['ps'] + args
-#     result = run(command, stdout=PIPE, stderr=PIPE, encoding='utf-8')
-#     output = result.stdout.strip()
-#     return f"<pre>{output}</pre>"
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
