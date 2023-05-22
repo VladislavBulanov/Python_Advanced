f637560456062e283d613977724b8c69fb157b60
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-22 17:31:39 +0300

    Backup: 2023-05-22_nb_ubuntu

--- module_04_flask/homework/hw5/ps.py
+++ module_04_flask/homework/hw5/ps.py
@@ -1,22 +1,45 @@
-"""
-Напишите GET-эндпоинт /ps, который принимает на вход аргументы командной строки,
-а возвращает результат работы команды ps с этими аргументами.
-Входные значения эндпоинт должен принимать в виде списка через аргумент arg.
-
-Например, для исполнения команды ps aux запрос будет следующим:
-
-/ps?arg=a&arg=u&arg=x
-"""
-
-from flask import Flask
+from flask import Flask, request
+from subprocess import run
+from typing import List
+import shlex
 
 app = Flask(__name__)
 
 
-@app.route("/ps", methods=["GET"])
-def ps() -> str:
-    ...
+@app.route('/ps', methods=['GET'])
+def execute_ps() -> str:
+    """The function returns result of performing 'ps' command."""
+
+    args_list: List[str] = request.args.getlist('arg')
+    args_string: str = shlex.quote("".join(args_list))
+    command_str = f"ps {args_string}"
+    command = shlex.split(command_str)
+    result = run(command, capture_output=True)
+    return f"<pre>{result}</pre>"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+# from flask import Flask, request
+# from subprocess import run, PIPE
+# from typing import List
+# import shlex
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/ps', methods=['GET'])
+# def execute_ps() -> str:
+#     args: List[str] = request.args.getlist('arg')
+#     command = ['ps'] + args
+#     result = run(command, stdout=PIPE, stderr=PIPE, encoding='utf-8')
+#     output = result.stdout.strip()
+#     return f"<pre>{output}</pre>"
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
