f9fa8ac35b6388a71dd301098d21f68b3d797ab0
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-24 23:52:32 +0300

    Done: task n.2

--- module_05_processes_and_threads/homework/hw2/remote_execution.py
+++ module_05_processes_and_threads/homework/hw2/remote_execution.py
@@ -1,30 +1,69 @@
-"""
-Напишите эндпоинт, который принимает на вход код на Python (строка)
-и тайм-аут в секундах (положительное число не больше 30).
-Пользователю возвращается результат работы программы, а если время, отведённое на выполнение кода, истекло,
-то процесс завершается, после чего отправляется сообщение о том, что исполнение кода не уложилось в данное время.
-"""
-
-from flask import Flask
+import subprocess
+from flask import Flask, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired, NumberRange
 
 app = Flask(__name__)
 
 
 class CodeForm(FlaskForm):
-    code = StringField()
-    timeout = IntegerField()
+    """The FlaskForm class."""
+
+    code = StringField('Code', validators=[
+        InputRequired("The field 'code' is required")
+    ])
+    timeout = IntegerField('Timeout', validators=[
+        InputRequired("The field 'timeout' is required"),
+        NumberRange(min=1, max=30)
+    ])
 
 
-def run_python_code_in_subproccess(code: str, timeout: int):
-    ...
+def run_python_code_in_subprocess(code: str, timeout: int):
+    """
+    The function takes Python code and timeout
+    and returns a result of performing of this program.
+    If execution time is more than timeout process is terminated
+    and user gets error message.
+    :param code: the Python code
+    :param timeout: timeout in seconds
+    """
+
+    command = ['python3', '-c', f'{code}']
+    # command = [
+    #     'prlimit', f'--cpu={timeout}', '--nproc=1:1', 'python3', '-c', code
+    # ]
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=False
+    )
+
+    try:
+        stdout, stderr = process.communicate(timeout=timeout)
+        result = stdout.decode('utf-8')
+
+    except subprocess.TimeoutExpired:
+        process.terminate()
+        process.wait(timeout=1)
+        result = 'Execution time exceeded the timeout limit.'
+
+    return result
 
 
 @app.route('/run_code', methods=['POST'])
 def run_code():
-    ...
+    form = CodeForm(request.form)
+    if form.validate_on_submit():
+        code = form.code.data
+        timeout = form.timeout.data
+        result = run_python_code_in_subprocess(code, timeout)
+        return result
+
+    return f"Invalid input, {form.errors}", 400
 
 
 if __name__ == '__main__':
+    app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
--- module_05_processes_and_threads/homework/hw2/test_remote_execution.py
+++ module_05_processes_and_threads/homework/hw2/test_remote_execution.py
@@ -1,5 +1,126 @@
 import unittest
+from flask_testing import TestCase
 from remote_execution import app
+
+
+class TestRemoteExecutionApp(TestCase):
+    """The class for testing 'Remote Execution' app."""
+
+    def create_app(self):
+        """
+        Create and configure the Flask app.
+        :return: the configured Flask app.
+        """
+
+        app.config['TESTING'] = True
+        app.config["WTF_CSRF_ENABLED"] = False
+        return app
+
+    def test_can_get_result_of_program_with_valid_data(self):
+        """The function checks if user can get
+        correct result of program with valid data."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'code': 'print("Hello, World!")',
+                'timeout': 5,
+            })
+            self.assert200(response)
+            self.assertEqual(response.data.decode(), "Hello, World!\n")
+
+    def test_cannot_run_program_without_code(self):
+        """The function checks if user cannot run
+        program with empty 'code' field."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'timeout': 5,
+            })
+            self.assert400(response)
+            self.assertIn(
+                "The field 'code' is required", response.data.decode()
+            )
+
+    def test_cannot_run_program_with_invalid_python_code(self):
+        """The function checks if user cannot run program
+        with invalid Python code in the 'code' field."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'code': 'print("Hello, World!"',
+                'timeout': 5,
+            })
+            self.assert200(response)
+            self.assertEqual("", response.data.decode())
+
+    def test_cannot_run_program_with_non_digital_timeout(self):
+        """The function checks if user cannot run program
+        if value of 'timeout' field is not a number."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'code': 'print("Hello, World!")',
+                'timeout': 'abc',
+            })
+            self.assert400(response)
+            self.assertIn(
+                "Not a valid integer value", response.data.decode()
+            )
+
+    def test_cannot_run_program_with_timeout_out_of_range(self):
+        """The function checks if user cannot run program
+        if value of 'timeout' is not in range specified in validator."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'code': 'print("Hello, World!")',
+                'timeout': 60,
+            })
+            self.assert400(response)
+            self.assertIn(
+                "Number must be between 1 and 30", response.data.decode()
+            )
+
+    def test_cannot_run_program_without_timeout(self):
+        """The function checks if user cannot run
+        program with empty 'timeout' field."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'code': 'print("Hello, World!")',
+            })
+            self.assert400(response)
+            self.assertIn(
+                "The field 'timeout' is required", response.data.decode()
+            )
+
+    def test_cannot_get_result_if_timeout_is_less_than_performing(self):
+        """The function checks if user cannot get of result of program
+        if timeout is less than time of program's execution."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'code': '999**1000000000000000000',
+                'timeout': 1,
+            })
+            self.assert200(response)
+            self.assertEqual(
+                "Execution time exceeded the timeout limit.", response.data.decode()
+            )
+
+    def test_cannot_run_program_with_shell_commands(self):
+        """The function checks if user cannot run program
+        with shell commands in the 'code' field."""
+
+        with self.app.test_client() as client:
+            response = client.post('/run_code', data={
+                'code': 'print("Hello, World!"); echo "hacked"',
+                'timeout': 5,
+            })
+            self.assert200(response)
+            self.assertEqual(
+                "", response.data.decode()
+            )
 
 
 if __name__ == '__main__':
