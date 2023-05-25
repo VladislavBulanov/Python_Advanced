e88c7accfa748d29090f1e8104214712893e6d02
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-25 23:11:58 +0300

    Done: task n.5

--- module_05_processes_and_threads/homework/hw5_add/self_printing.py
+++ module_05_processes_and_threads/homework/hw5_add/self_printing.py
@@ -6,12 +6,10 @@ for n in range(1, 11):
     result += n ** 2
 
 
-# Получаем путь к текущему файлу
+# Get path to the current file:
 file_path = inspect.getfile(inspect.currentframe())
 
-# Открываем файл и читаем его содержимое
 with open(file_path, 'r', encoding='utf-8') as file:
     code = file.read()
 
-# Выводим содержимое файла
 print(code)
