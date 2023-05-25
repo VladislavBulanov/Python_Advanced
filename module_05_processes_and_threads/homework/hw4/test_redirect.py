3850b1e52307fa6f2a5611e66d876884422584a3
Vladislav Bulanov <solist95@mail.ru>
2023-05-25 17:59:01 +0300

    Backup: 2023-05-25_nb_win10

--- module_05_processes_and_threads/homework/hw4/test_redirect.py
+++ module_05_processes_and_threads/homework/hw4/test_redirect.py
@@ -1,9 +1,62 @@
+import sys
 import unittest
+from io import StringIO
 from redirect import Redirect
 
 
+class RedirectTestCase(unittest.TestCase):
+    """The class for testing threads' redirect program."""
+
+    def test_redirect_stdout_only(self):
+        # Redirect stdout only, stderr remains unchanged
+
+        original_stderr = sys.stderr
+        output = StringIO()
+        with Redirect(stdout=output):
+            print('Hello stdout')
+            raise Exception('Hello stderr')
+        self.assertEqual(output.getvalue().strip(), 'Hello stdout')
+        self.assertEqual(sys.stderr, original_stderr)
+
+    def test_redirect_stderr_only(self):
+        # Redirect stderr only, stdout remains unchanged
+
+        original_stdout = sys.stdout
+        output = StringIO()
+        with Redirect(stderr=output):
+            print('Hello stdout')
+            raise Exception('Hello stderr')
+        self.assertEqual(sys.stdout, original_stdout)
+        self.assertIn('Exception: Hello stderr', output.getvalue().strip())
+
+    def test_both_redirected(self):
+        # Redirect stdout and stderr to separate StringIO objects
+
+        stdout_output = StringIO()
+        stderr_output = StringIO()
+        with Redirect(stdout=stdout_output, stderr=stderr_output):
+            print('Hello stdout')
+            raise Exception('Hello stderr')
+        self.assertEqual(stdout_output.getvalue().strip(), 'Hello stdout')
+        self.assertIn(
+            'Exception: Hello stderr', stderr_output.getvalue().strip()
+        )
+
+    def test_no_redirect(self):
+        # No redirection
+
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+
+        with Redirect():
+            print('Hello stdout')
+            raise Exception('Hello stderr')
+
+        self.assertIs(sys.stdout, original_stdout)
+        self.assertIs(sys.stderr, original_stderr)
+
+
 if __name__ == '__main__':
-    unittest.main()
-    # with open('test_results.txt', 'a') as test_file_stream:
-    #     runner = unittest.TextTestRunner(stream=test_file_stream)
-    #     unittest.main(testRunner=runner)
+    with open('test_results.txt', 'w') as test_file_stream:
+        runner = unittest.TextTestRunner(stream=test_file_stream)
+        unittest.main(testRunner=runner)
--- module_05_processes_and_threads/homework/hw5_add/self_printing.py
+++ module_05_processes_and_threads/homework/hw5_add/self_printing.py
@@ -1,10 +1,17 @@
-"""
-Напишите код, который выводит сам себя.
-Обратите внимание, что скрипт может быть расположен в любом месте.
-"""
+import inspect
+
 
 result = 0
 for n in range(1, 11):
     result += n ** 2
 
-# Secret magic code
+
+# Получаем путь к текущему файлу
+file_path = inspect.getfile(inspect.currentframe())
+
+# Открываем файл и читаем его содержимое
+with open(file_path, 'r', encoding='utf-8') as file:
+    code = file.read()
+
+# Выводим содержимое файла
+print(code)
