35b247841092143ced5e66850b1a1134ce6aaa08
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-28 13:39:45 +0300

    Backup: 2023-05-28_nb_ubuntu

--- module_06_debugging_begin/homework/hw1_2/authentication.py
+++ module_06_debugging_begin/homework/hw1_2/authentication.py
@@ -6,12 +6,23 @@ logger = logging.getLogger("password_checker")
 
 
 def is_strong_password(password: str) -> bool:
+    """
+    The function checks if password is strong.
+    :param password: the password that user inputted
+    :return: True if password is strong, False if password is weak
+    """
+
     return True
 
 
 def input_and_check_password() -> bool:
+    """
+    The function requests password from user and verifies it.
+    :return: True if password is valid, False if it doesn't
+    """
+
     logger.debug("Начало input_and_check_password")
-    password: str = getpass.getpass()
+    password: str = getpass.getpass("Enter password: ")
 
     if not password:
         logger.warning("Вы ввели пустой пароль.")
@@ -27,8 +38,9 @@ def input_and_check_password() -> bool:
 
         if hasher.hexdigest() == "098f6bcd4621d373cade4e832627b4f6":
             return True
-    except ValueError as ex:
-        logger.exception("Вы ввели некорректный символ ", exc_info=ex)
+
+    except ValueError as error:
+        logger.exception("Вы ввели некорректный символ ", exc_info=error)
 
     return False
 
@@ -36,9 +48,10 @@ def input_and_check_password() -> bool:
 if __name__ == "__main__":
     logging.basicConfig(filename='stderr.txt',
                         filemode='a',
-                        format='%(asctime)s %(name)s %(levelname)s %(message)s',
+                        format='%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s',
                         datefmt='%H:%M:%S',
                         level=logging.INFO)
+
     logger.info("Вы пытаетесь аутентифицироваться в Skillbox")
     count_number: int = 3
 
