f7ddb3371cffa42c8ec3aa7d3654872df78376aa
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-30 02:27:12 +0300

    Backup: nb_ubuntu

--- module_06_debugging_begin/homework/hw5/measure_me.py
+++ module_06_debugging_begin/homework/hw5/measure_me.py
@@ -1,16 +1,26 @@
-"""
-Каждый лог содержит в себе метку времени, а значит, правильно организовав логирование,
-можно отследить, сколько времени выполняется функция.
-
-Программа, которую вы видите, по умолчанию пишет логи в stdout. Внутри неё есть функция measure_me,
-в начале и в конце которой пишется "Enter measure_me" и "Leave measure_me".
-Сконфигурируйте логгер, запустите программу, соберите логи и посчитайте среднее время выполнения функции measure_me.
-"""
+import json
 import logging
 import random
+from datetime import datetime
 from typing import List
 
-logger = logging.getLogger(__name__)
+
+class JsonAdapter(logging.LoggerAdapter):
+    """The class for create an instance of LoggerAdapter
+    initialized with an underlying Logger instance
+    and a dict-like object."""
+
+    def process(self, message, kwargs):
+        """
+        Modifies the message and/or keyword arguments
+        passed to a logging call in order to insert
+        contextual information.
+        :param message: source message
+        :param kwargs: kwargs
+        :return: JSON-message
+        """
+        json_message = json.dumps(message)
+        return json_message, kwargs
 
 
 def get_data_line(sz: int) -> List[int]:
@@ -22,7 +32,7 @@ def get_data_line(sz: int) -> List[int]:
 
 
 def measure_me(nums: List[int]) -> List[List[int]]:
-    logger.debug("Enter measure_me")
+    logger.info("Enter measure_me")
     results = []
     nums.sort()
 
@@ -54,13 +64,35 @@ def measure_me(nums: List[int]) -> List[List[int]]:
 
                     right -= 1
 
-    logger.debug("Leave measure_me")
+    logger.info("Leave measure_me")
 
     return results
 
 
+def get_average_time_of_function_performing(filename='logs.log') -> float:
+    """The function returns average time of function performing via logs."""
+
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            logs = [json.loads(line) for line in file]
+
+    except FileNotFoundError:
+        print("Source file doesn't exist")
+
+
 if __name__ == "__main__":
-    logging.basicConfig(level="DEBUG")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(filename='logs.log', mode='w')
+    formatter = logging.Formatter(
+        '{"time": "%(asctime)s", "level": "%(levelname)s", "message": %(message)s}'
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger = JsonAdapter(logger)
+
     for it in range(15):
         data_line = get_data_line(10 ** 3)
         measure_me(data_line)
+
+    print(get_average_time_of_function_performing())
