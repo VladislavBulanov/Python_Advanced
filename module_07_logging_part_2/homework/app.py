897f198e0d52cddf49c9be7eded434f5f584d2b8
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-06-02 10:32:14 +0300

    Done: task n.1

--- module_07_logging_part_2/homework/app.py
+++ module_07_logging_part_2/homework/app.py
@@ -1,9 +1,14 @@
+import logging
 import sys
 from utils import string_to_operator
 
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("app")
+
+
 def calc(args):
-    print("Arguments: ", args)
+    logger.debug(f"Arguments: {args}")
 
     num_1 = args[0]
     operator = args[1]
@@ -12,21 +17,21 @@ def calc(args):
     try:
         num_1 = float(num_1)
     except ValueError as e:
-        print("Error while converting number 1")
-        print(e)
+        logger.error("Error while converting number 1")
+        logger.error(e)
 
     try:
         num_2 = float(num_2)
     except ValueError as e:
-        print("Error while converting number 1")
-        print(e)
+        logger.error("Error while converting number 2")
+        logger.error(e)
 
     operator_func = string_to_operator(operator)
 
     result = operator_func(num_1, num_2)
 
-    print("Result: ", result)
-    print(f"{num_1} {operator} {num_2} = {result}")
+    logger.info(f"Result: {result}")
+    logger.info(f"{num_1} {operator} {num_2} = {result}")
 
 
 if __name__ == '__main__':
--- module_07_logging_part_2/homework/utils.py
+++ module_07_logging_part_2/homework/utils.py
@@ -1,6 +1,10 @@
+import logging
 from typing import Union, Callable
 from operator import sub, mul, truediv, add
 
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("utils")
 
 OPERATORS = {
     '+': add,
@@ -17,12 +21,13 @@ def string_to_operator(value: str) -> Callable[[Numeric, Numeric], Numeric]:
     Convert string to arithmetic function
     :param value: basic arithmetic function
     """
+
     if not isinstance(value, str):
-        print("wrong operator type", value)
+        logger.error(f"wrong operator type {value}")
         raise ValueError("wrong operator type")
 
     if value not in OPERATORS:
-        print("wrong operator value", value)
+        logger.error(f"wrong operator value {value}")
         raise ValueError("wrong operator value")
 
     return OPERATORS[value]
