60a87b1f410d29867e9e3f87fec8c250e3f9e315
Vladislav Bulanov <solist95@mail.ru>
2023-06-18 23:48:40 +0300

    Done: task n.5

--- module_10_db1/homework/hw5/quick_insert.py
+++ module_10_db1/homework/hw5/quick_insert.py
@@ -4,7 +4,26 @@ Number = Union[int, float, complex]
 
 
 def find_insert_position(array: List[Number], number: Number) -> int:
-    ...
+    """
+    The function returns an index showing
+    where the specified number needs to be inserted
+    in order for the array to remain sorted.
+    The function uses binary search.
+    :param array: source array
+    :param number: inputted number
+    """
+
+    left = 0
+    right = len(array)
+
+    while left < right:
+        mid = (left + right) // 2
+        if array[mid] < number:
+            left = mid + 1
+        else:
+            right = mid
+
+    return left
 
 
 if __name__ == '__main__':
@@ -15,5 +34,35 @@ if __name__ == '__main__':
 
     A: List[Number] = [1, 2, 3, 3, 3, 5]
     x: Number = 4
+    A.insert(insert_position, x)
+    assert A == sorted(A)
+
+    A: List[Number] = []
+    x: Number = 4
+    insert_position: int = find_insert_position(A, x)
+    assert insert_position == 0
+
+    A: List[Number] = []
+    x: Number = 4
+    A.insert(insert_position, x)
+    assert A == sorted(A)
+
+    A: List[Number] = [1, 2, 3, 3, 3, 5]
+    x: Number = -1
+    insert_position: int = find_insert_position(A, x)
+    assert insert_position == 0
+
+    A: List[Number] = [1, 2, 3, 3, 3, 5]
+    x: Number = -1
+    A.insert(insert_position, x)
+    assert A == sorted(A)
+
+    A: List[Number] = [1, 2, 3, 3, 3, 5]
+    x: Number = 10
+    insert_position: int = find_insert_position(A, x)
+    assert insert_position == 6
+
+    A: List[Number] = [1, 2, 3, 3, 3, 5]
+    x: Number = 10
     A.insert(insert_position, x)
     assert A == sorted(A)
