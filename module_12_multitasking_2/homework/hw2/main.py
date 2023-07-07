94b738cd8f050628ed43822b12c4bb17caed0727
Vladislav Bulanov <solist95@mail.ru>
2023-07-07 06:06:18 +0300

    Backup: nb_win10

--- module_12_multitasking_2/homework/hw2/main.py
+++ module_12_multitasking_2/homework/hw2/main.py
@@ -1,10 +1,15 @@
-def process_count(username: str) -> int:
-    # количество процессов, запущенных из-под
-    # текущего пользователя username
-    pass
+import subprocess
 
+def process_count(username: str) -> int:
+    # Количество процессов, запущенных из-под текущего пользователя username
+    command = f'ps -u {username} -o pid --no-headers | wc -l'
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    count = int(result.stdout.strip())
+    return count
 
 def total_memory_usage(root_pid: int) -> float:
-    # суммарное потребление памяти древа процессов
-    # с корнем root_pid в процентах
-    pass
+    # Суммарное потребление памяти древа процессов с корнем root_pid в процентах
+    command = f'pgrep -P {root_pid} | xargs ps -o rss --no-headers | awk \'{{sum+=$1}} END {{print sum}}\''
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    memory_usage = float(result.stdout.strip())
+    return memory_usage
