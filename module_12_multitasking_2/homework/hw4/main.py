def823e8e4449af7a9ac0d197a14cb4722d72a9a
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-07-13 01:03:42 +0300

    Done: task n.4

--- module_12_multitasking_2/homework/hw4/main.py
+++ module_12_multitasking_2/homework/hw4/main.py
@@ -3,23 +3,32 @@ import logging
 import threading
 import time
 from multiprocessing import Queue
-from typing import Optional
+from typing import Optional, List, Dict
 
 import requests
 
-URL: str = 'http://127.0.0.1:8080/timestamp/{}'
+
+URL: str = "http://127.0.0.1:8080/timestamp/{}"
 WORKERS_COUNT: int = 10
 WORKER_REQUESTS: int = 20
 
 queue: Queue = Queue(WORKERS_COUNT * WORKER_REQUESTS)
-data: dict[float, Optional[str]] = {}
+data: Dict[float, Optional[str]] = {}
 
 
 def get_date(timestamp: float) -> str:
+    """
+    The function returns date by specified timestamp.
+    :param timestamp: the source timestamp
+    """
+
     return requests.get(URL.format(timestamp)).text
 
 
 class Worker(threading.Thread):
+    """The child class of 'threading.Thread'
+    class describing the one thread."""
+
     def run(self) -> None:
         for _ in range(WORKER_REQUESTS):
             timestamp: float = datetime.datetime.now().timestamp()
@@ -32,22 +41,24 @@ class Worker(threading.Thread):
             time.sleep(1)
 
 
-if __name__ == '__main__':
+def main() -> None:
+    """The main function of the app."""
+
     logging.basicConfig(
         level='INFO',
         filename='sorted_logs.log',
-        format='%(asctime)s | %(message)s'
+        format='%(asctime)s | %(message)s',
     )
 
-    threads: list[Worker] = []
+    threads: List[Worker] = []
 
-    for i in range(WORKERS_COUNT):
+    for _ in range(WORKERS_COUNT):
         thread: Worker = Worker()
         thread.start()
         threads.append(thread)
         time.sleep(1)
 
-    for i in range(queue.qsize()):
+    for _ in range(queue.qsize()):
         timestamp: float = queue.get()
         while data.get(timestamp) is None:
             continue
@@ -55,3 +66,7 @@ if __name__ == '__main__':
 
     for thread in threads:
         thread.join()
+
+
+if __name__ == '__main__':
+    main()
