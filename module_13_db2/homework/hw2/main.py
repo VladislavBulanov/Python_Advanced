98352894f2c796e3e8262cbffc114f20d73af1a7
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-07-14 19:40:24 +0300

    Done: task n.2

--- module_13_db2/homework/hw2/main.py
+++ module_13_db2/homework/hw2/main.py
@@ -1,15 +1,40 @@
+import csv
 import sqlite3
+
+
+SQL_QUERY_TO_DELETE = """
+    DELETE FROM `table_fees`
+        WHERE truck_number = ? AND timestamp = ?
+"""
 
 
 def delete_wrong_fees(
         cursor: sqlite3.Cursor,
-        wrong_fees_file: str
+        wrong_fees_file: str,
 ) -> None:
-    ...
+    """
+    The function deletes the wrong fees in the source database.
+    The wrong fees are specified in CSV-file.
+    :param cursor: the source sqlite3.Cursor
+    :param wrong_fees_file: the CSV-file with wrong fees
+    """
+
+    with open(wrong_fees_file) as csvfile:
+        fees_data = csv.reader(csvfile)
+
+        for record in fees_data:
+            car_number, timestamp = record
+            cursor.execute(SQL_QUERY_TO_DELETE, (car_number, timestamp))
 
 
-if __name__ == "__main__":
+def main() -> None:
+    """The main function of the app."""
+
     with sqlite3.connect("../homework.db") as conn:
         cursor: sqlite3.Cursor = conn.cursor()
         delete_wrong_fees(cursor, "../wrong_fees.csv")
         conn.commit()
+
+
+if __name__ == "__main__":
+    main()
