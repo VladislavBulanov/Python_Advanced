cc991ac4098b0bdfa56e9e55f770692670bdce06
Vladislav Bulanov <solist95@mail.ru>
2023-08-06 16:59:14 +0300

    Done: task n.2_2

--- module_16_db3/homework/hw2/2_2.py
+++ module_16_db3/homework/hw2/2_2.py
@@ -1,11 +1,19 @@
 import sqlite3
 
-SQL_SCRIPT: str = """
 
+SQL_QUERY: str = """
+    SELECT customer.full_name
+    FROM `customer` customer
+    LEFT OUTER JOIN `order` ord
+    ON customer.customer_id = ord.customer_id
+    WHERE ord.customer_id IS NULL;
 """
+
 
 if __name__ == "__main__":
     with sqlite3.connect("../hw.db") as conn:
         cursor: sqlite3.Cursor = conn.cursor()
-        cursor.executescript(SQL_SCRIPT)
+        result = cursor.execute(SQL_QUERY).fetchall()
+        for row in result:
+            print(*row)
         conn.commit()
