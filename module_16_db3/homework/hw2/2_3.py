2689dc662c708b9663ccb79c75b70e115644d594
Vladislav Bulanov <solist95@mail.ru>
2023-08-06 18:28:27 +0300

    Done: task n.2_3

--- module_16_db3/homework/hw2/2_3.py
+++ module_16_db3/homework/hw2/2_3.py
@@ -1,11 +1,29 @@
 import sqlite3
 
-SQL_SCRIPT: str = """
-
+SQL_QUERY: str = """
+    SELECT ord.order_no,
+           manager.full_name,
+           customer.full_name
+    FROM `order` ord
+    
+    INNER JOIN `customer` customer
+    ON ord.customer_id = customer.customer_id
+    
+    INNER JOIN `manager` manager
+    ON ord.manager_id = manager.manager_id
+    
+    WHERE customer.city <> manager.city;
 """
+
 
 if __name__ == "__main__":
     with sqlite3.connect("../hw.db") as conn:
         cursor: sqlite3.Cursor = conn.cursor()
-        cursor.executescript(SQL_SCRIPT)
+        result = cursor.execute(SQL_QUERY).fetchall()
+        for row in result:
+            print(
+                "Order number: {}, manager: {}, customer: {}".format(
+                    row[0], row[1], row[2],
+                )
+            )
         conn.commit()
