6af908f3af2bc17729552735470fc3d4f9e54e05
Vladislav Bulanov <solist95@mail.ru>
2023-08-06 17:34:29 +0300

    Fix_hw2/2_1.py: the SQL script is changed

--- module_16_db3/homework/hw2/2_1.py
+++ module_16_db3/homework/hw2/2_1.py
@@ -7,8 +7,12 @@ SQL_QUERY: str = """
            ord.purchase_amount,
            ord.date
     FROM `order` ord
-    LEFT OUTER JOIN `customer` customer ON ord.customer_id = customer.customer_id
-    LEFT OUTER JOIN `manager` manager ON ord.manager_id = manager.manager_id;
+    
+    INNER JOIN `customer` customer
+    ON ord.customer_id = customer.customer_id
+    
+    LEFT OUTER JOIN `manager` manager
+    ON customer.manager_id = manager.manager_id;
 """
 
 
@@ -16,7 +20,6 @@ if __name__ == "__main__":
     with sqlite3.connect("../hw.db") as conn:
         cursor: sqlite3.Cursor = conn.cursor()
         result = cursor.execute(SQL_QUERY).fetchall()
-        print(len(result))
         for row in result:
             print(
                 "Customer: {}, manager: {}, purchase amount: {}, date: {}".format(
